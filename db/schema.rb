6f96479cc3fedd9836ae4e80eff38d967c4207a9
Myat Thu Hein <myatthuhein97@gmail.com>
2016-10-22 10:23:39 +0000

    Added plan model and db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021033732) do
+ActiveRecord::Schema.define(version: 20161022100619) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -38,5 +38,12 @@ ActiveRecord::Schema.define(version: 20161021033732) do
 
   add_index "models", ["email"], name: "index_models_on_email", unique: true
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+
+  create_table "plans", force: true do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
